2d0d959081333e47a16dfe85fd8fdc30286c8997
Jon Winton <jwinton@squareup.com>
2021-11-05 13:21:32 -0500

    bump version

--- cmd/yaae/main.go
+++ cmd/yaae/main.go
@@ -36,7 +36,7 @@ func main() {
 			Compact: true,
 		}),
 		kong.Vars{
-			"version":     "0.0.1",
+			"version":     "0.0.2",
 			"log_levels":  strings.Join(logLevels, ","),
 			"config_file": "yaae.yaml",
 		})
